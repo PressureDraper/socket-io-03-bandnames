6f8001b2b2fb1d5197a6690af50a088317368a55
PressureDraper <rojeru.san1983@gmail.com>
2024-12-07 22:59:21 -0600

    fix: .env var names to be recognized

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : process.env.SOCKET_SERVER_PORT;
+const URL = process.env.REACT_APP_NODE_ENVIRONMENT === 'production' ? undefined : `http://localhost:${process.env.REACT_APP_SOCKET_SERVER_PORT}`;
 
 export const socketConnection = io(URL, { autoConnect: false, transports: ['websocket'] });
