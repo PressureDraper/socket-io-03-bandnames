fef3e615a238084c36b4e506466fa5d5dc057a83
PressureDraper <rojeru.san1983@gmail.com>
2024-12-07 21:54:19 -0600

    add: .env var to socket server

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:9191';
+const URL = process.env.NODE_ENV === 'production' ? undefined : process.env.SOCKET_SERVER_PORT;
 
 export const socketConnection = io(URL, { autoConnect: false, transports: ['websocket'] });
